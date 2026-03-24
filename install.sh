da5a5cb10763893fdc463298eaca14534e740253
Kieran Smith <kieran.smith@itv.com>
2026-03-24 17:34:31 +0000

    Add brewfile

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# curl -L https://dtinth.github.io/comic-mono-font/ComicMono.ttf -o /Library/Fonts/ComicMono.ttf
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -L https://dtinth.github.io/comic-mono-font/ComicMono.ttf -o /Library/Fonts/ComicMono.ttf
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-brew install zsh-autosuggestions zsh-syntax-highlighting awscli thefuck ripgrep catimg wget eza
+brew bundle --file="~/.dotfiles/Brewfile"
 
 echo "Creating symlinks..."
 ln -sfv ~/.dotfiles/gitconfig ~/.gitconfig
